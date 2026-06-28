2d2c75b72a71a8f00547493faa5bf37b852a8224
Stefano Franzoni <stefano.franzoni@gmail.com>
2026-06-28 17:56:17 +0200

    fix: base-href corretto per GitHub Pages

--- docs/web/flutter_bootstrap.js
+++ docs/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1470319190" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4058397250" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
